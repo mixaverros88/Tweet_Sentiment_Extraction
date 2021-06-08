c9bf1155413104d6725e592bc4de5491968f4891
mverros <mixalisverros@hotmail.gr>
2021-06-08 14:31:37 +0300

    add one more test for preprocessing

--- test/data_preprocessing/test_data_preprocessing.py
+++ test/data_preprocessing/test_data_preprocessing.py
@@ -10,3 +10,13 @@ sample_cleaning_dataset = DataCleaning(data_frame, 'textID', 'request')
 cleaned_sample_data_frame = sample_cleaning_dataset.data_cleaning()
 
 assert cleaned_sample_data_frame.loc[0, 'text'] == 'hello he said and went'
+
+# data
+text = 'My name is mike !!!!   '
+data = [[1, text, '', 'positive']]
+data_frame = pd.DataFrame(data, columns=['textID', 'text', 'selected_text', 'sentiment'])
+
+sample_cleaning_dataset = DataCleaning(data_frame, 'textID', 'request')
+cleaned_sample_data_frame = sample_cleaning_dataset.data_cleaning()
+
+assert cleaned_sample_data_frame.loc[0, 'text'] == 'my name is mike'
